fa24212a18700be8f796d0efda83fbc8f90da169
Gophix <61445366+G0phix@users.noreply.github.com>
2026-03-09 21:54:34 -0300

    adicionado entrega.txt

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,8 @@ import 'react-native-reanimated';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Alert, Button, Platform } from 'react-native';
+import React from 'react';
+import { Alert, Button, Platform, TextInput } from 'react-native';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -12,6 +13,8 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  // effect
+  const [nome, setNome] = React.useState('Raimundo');
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
@@ -30,9 +33,24 @@ export default function RootLayout() {
         >
           Hello World!
         </ThemedText>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            backgroundColor: 'lightgray',
+            borderWidth: 1,
+            width: '80%',
+            paddingHorizontal: 8,
+            marginBottom: 16,
+          }}
+          value={nome}
+          onChangeText={setNome}
+           placeholder="Digite algo aqui..."
+          >
+        </TextInput>
         <Button 
           title='Aperte aqui'
-          onPress={() => Alert.alert('Informação:', `Olá ${Platform.OS}! Eu me chamo Raimundo`)}
+          onPress={() => Alert.alert('Informação:', `Olá ${nome}! você usa ${Platform.OS}.`)}
         />
       </ThemedView>
     </ThemeProvider>
